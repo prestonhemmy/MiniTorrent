b90c518a44fd80195910e1c0ed399439fb4cb15d
prestonhemmy <phemmy@ufl.edu>
2025-12-02 15:54:12 -0500

    chore: update reset.sh

--- reset.sh
+++ reset.sh
@@ -5,5 +5,10 @@
 rm -f log_peer_*.log
 find src/project_config_file_large -name "piece_*.dat" -delete
 rm -f src/project_config_file_large/1002/tree.jpg
+rm -f src/project_config_file_large/1003/tree.jpg
+rm -f src/project_config_file_large/1004/tree.jpg
+rm -f src/project_config_file_large/1005/tree.jpg
+rm -f src/project_config_file_large/1006/tree.jpg
+
 
 echo "Cleaning test environment complete."
